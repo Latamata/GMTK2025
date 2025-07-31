2358b5bc9cd5ba70a2564f2fedaa88c1d742aba8
Latamata <chancewebber@gmail.com>
2025-07-30 19:49:41 -0700

    Minor changes
    setup the pause and unpause for getting object to stop, and continue moving.

--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -19,3 +19,22 @@ func _on_area_2d_3_body_entered(body: Node2D) -> void:
 func _on_area_2d_4_body_entered(body: Node2D) -> void:
 	if body is RigidBody2D:
 		body.apply_central_impulse(Vector2.UP * push_force)
+
+@onready var rigid_body_2d: RigidBody2D = $RigidBody2D
+@onready var rigid_body_2d_2: RigidBody2D = $RigidBody2D2
+
+
+
+
+@onready var areas := [
+	$Area2D,
+	$Area2D2,
+	$Area2D3,
+	$Area2D4
+]
+
+func _on_button_button_down() -> void:
+	get_tree().paused = true
+
+func _on_button_2_button_down() -> void:
+	get_tree().paused = false
